f9469acd56862932c9c754c8a960c9d02e0860a5
aistomin <andrej.istomin.ikeen@gmail.com>
2016-03-20 17:56:46 +0100

    BootStrap.groovy: test user initialization was added.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,23 @@
+import org.aistomin.chat.model.UserRecord
+import org.aistomin.chat.model.UserRecordUserRoleRecord
+import org.aistomin.chat.model.UserRoleRecord
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (UserRoleRecord.count == 0) {
+            new UserRoleRecord(authority: 'ROLE_USER').save()
+        }
+        if (UserRecord.count == 0) {
+            final def user = new UserRecord(
+                name: 'My User', username: 'test', password: 'test'
+            ).save()
+            new UserRecordUserRoleRecord(
+                user, UserRoleRecord.findByAuthority('ROLE_USER')
+            ).save()
+        }
     }
+
     def destroy = {
     }
 }
